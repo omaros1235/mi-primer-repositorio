320961487fdd568a230572a46b4e2b08597a7f4a
KLEBER MORALES <ko.moralesl@uea.edu.ec>
2024-02-27 21:00:18 -0500

    debersemana 11

--- ejercicios/matris 3x3.py
+++ ejercicios/matris 3x3.py
@@ -1,4 +1,4 @@
-# matriz bidimensional de 3x3
+# Definir una matriz bidimensional de 3x3
 matriz = [
     [1, 2, 3],
     [4, 5, 6],
@@ -10,12 +10,10 @@ print("Matriz original:")
 for fila in matriz:
     print(fila)
 
-# Acceder a elementos específicos de la matriz
-elemento = matriz[1][2]
-print("\nElemento en la segunda fila y tercera columna:", elemento)
+# Modificar un valor en la matriz (por ejemplo, cambiar el valor en la segunda fila y tercera columna)
+matriz[1][2] = 10
 
-# Modificar un valor en la matriz
-matriz[0][0] = 10
-print("\nMatriz después de modificar el primer elemento:")
+# Imprimir la matriz después de la modificación
+print("\nMatriz modificada:")
 for fila in matriz:
     print(fila)
